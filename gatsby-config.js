ecfb6041d853998dab8fa7318e31a06f85d2a998
manan0308 <63553691+manan0308@users.noreply.github.com>
2020-11-08 20:39:31 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,9 +45,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `Manan Agarwal`,
+        short_name: `Manan Agarwal`,
+        description: `I build stuff and share my learnings`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
