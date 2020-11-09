347ae619dc7d3384d4b26aec9a4b9d81fe27d605
manan0308 <63553691+manan0308@users.noreply.github.com>
2020-11-09 10:30:21 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
+    siteTitle: `Manan Agarwal`,
     siteTitleAlt: `Manan Agarwal`,
   },
   plugins: [
