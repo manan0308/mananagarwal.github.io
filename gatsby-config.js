a63734f167cdae4ef866b0ed37343878ab982c89
manan0308 <63553691+manan0308@users.noreply.github.com>
2020-11-11 00:17:25 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
     siteTitleAlt: `Manan Agarwal`,
     siteHeadline: `Manan Agarwal`,
     siteDescription: `I build stuff on the internet for the internet and share my learnings`,
+    siteImage: "/banner.png",
   },
   plugins: [
     {
