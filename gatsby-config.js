37b89cf10213bc8b7c47f0c4bd33cf4afcc3f490
manan0308 <63553691+manan0308@users.noreply.github.com>
2023-05-07 10:51:07 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,43 @@
 require(`dotenv`).config({
   path: `.env`,
-});
+})
 
-const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
-    // ...
+    siteTitle: `Manan Agarwal`,
+    siteTitleAlt: `Manan Agarwal`,
+    siteHeadline: `Manan Agarwal`,
+    siteDescription: `I build stuff on the internet, for the internet and share my learnings.`,
+    siteImage: "/mananpicture.jpg",
   },
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-minimal-blog`,
-      // ...
+      // See the theme's README for all available options
+      options: {
+        navigation: [
+          {
+            title: `Blog`,
+            slug: `/blog`,
+          },
+          {
+            title: `About`,
+            slug: `/about`,
+          },
+        ],
+        externalLinks: [
+          {
+            name: `Twitter`,
+            url: `https://twitter.com/manan_0308`,
+          },
+          {
+            name: `LinkedIn`,
+            url: `https://www.linkedin.com/in/mananagarwal03`,
+          },
+        ],
+      },
     },
     {
       resolve: `gatsby-plugin-google-gtag`,
@@ -31,7 +57,25 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        // ...
+        name: `Manan Agarwal`,
+        short_name: `Manan Agarwal`,
+        description: `I build stuff and share my learnings`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#6B46C1`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
       },
     },
     `gatsby-plugin-offline`,
@@ -45,4 +89,4 @@ module.exports = {
       },
     },
   ].filter(Boolean),
-};
+}
